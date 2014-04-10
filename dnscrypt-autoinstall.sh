6dbde5c36041a2783ad534e1069f83599c865052
AladW <the.changing.side@gmail.com>
2014-04-11 01:16:34 +0200

    dnscrypt user unable to chroot
    While making a few changes to my network, I've noticed DNSCrypt hasn't been run for three days. 
    
    Launching it via /etc/init.d/dnscrypt-proxy or dnscrypt-proxy -d -u dnscrypt gave no errors, but no launched process either. 
    
    /var/log/daemon.log
    
    ```Apr  8 02:29:43 arbellen dnscrypt-proxy[2112]: Generating a new key pair
    Apr  8 02:29:43 arbellen dnscrypt-proxy[2112]: Done
    Apr  8 02:29:44 arbellen dnscrypt-proxy[2112]: Unable to chroot to [/var/run/dnscrypt]```
    
    ls -l /var/run
    
    ```lrwxrwxrwx 1 root root 4 apr  6 20:12 /var/run -> /run```
    
    Deleting the user and making a new one with a /run home made it work again. I've also replaced useradd with adduser (man useradd mentions to) and added some safety checks (/bin/false, disabled pw/login)

--- dnscrypt-autoinstall.sh
+++ dnscrypt-autoinstall.sh
@@ -203,6 +203,7 @@ else
 		
 		# Add dnscrypt user and homedir
 		mkdir -p /var/run/dnscrypt
+		adduser --system --home /run/dnscrypt --shell /bin/false --group --disabled-password --disabled-login dnscrypt
 		useradd -d /var/run/dnscrypt --system dnscrypt
 		chown dnscrypt /var/run/dnscrypt
 		
