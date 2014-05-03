979781738b04ed430ebffc35917671657ba8201c
theodorerambert <theodorerambert@users.noreply.github.com>
2014-05-03 19:30:24 -0400

    Update initscript-opendns.sh
    Added -R option for OpenDNS

--- init-scripts/initscript-opendns.sh
+++ init-scripts/initscript-opendns.sh
@@ -14,17 +14,11 @@
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 DAEMON=/usr/local/sbin/dnscrypt-proxy
 NAME=dnscrypt-proxy
-ADDRESS1=208.67.220.220
-ADDRESS2=208.67.222.123
-PNAME1=2.dnscrypt-cert.opendns.com
-PNAME2=2.dnscrypt-cert.opendns.com
-PKEY=B735:1140:206F:225D:3E2B:D822:D7FD:691E:A1C3:3CC8:D666:8D0C:BE04:BFAB:CA43:FB79
 
 case "$1" in
   start)
     echo "Starting $NAME"
-    $DAEMON --daemonize --user=dnscrypt --local-address=127.0.0.1 --resolver-address=$ADDRESS1 --provider-name=$PNAME1 --provider-key=$PKEY
-$DAEMON --daemonize --user=dnscrypt --local-address=127.0.0.2 --resolver-address=$ADDRESS2 --provider-name=$PNAME2 --provider-key=$PKEY
+    $DAEMON --daemonize --user=dnscrypt -R OpenDNS
     ;;
   stop)
     echo "Stopping $NAME"
