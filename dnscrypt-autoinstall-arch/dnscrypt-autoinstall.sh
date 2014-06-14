53c9e841b7c323086b271b20647c313c2978482d
AladW <the.changing.side@gmail.com>
2014-06-14 08:45:38 +0200

    Update dnscrypt-autoinstall.sh

--- dnscrypt-autoinstall-arch/dnscrypt-autoinstall.sh
+++ dnscrypt-autoinstall-arch/dnscrypt-autoinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ### 
-# Installation and autoconfigure script for Arch Linux and dnscrypt.
+# Autoconfigure script for Arch Linux and dnscrypt.
 #
-# This script will install dnscrypt and set it up as a daemon service that runs on
-# system startup. It also gives you the option to choose which DNSCrypt service to use 
-# and easily reconfigure DNSCrypt and uninstall it.
+# This script will set up dnscrypt as a daemon service that runs on
+# system startup. It gives you the option to choose which DNSCrypt service to use 
+# and easily reconfigure DNSCrypt.
 #
 # Author: Simon Clausen <kontakt@simonclausen.dk>
 # Version: 0.3
@@ -20,8 +20,6 @@ if (( UID = 0 )); then
 fi
 
 # Vars for stuff
-LSODIUMINST=false
-DNSCRYPTINST=false
 WHICHRESOLVER=dnscrypteu
 GITURL=https://raw.github.com/simonclausen/dnscrypt-autoinstall/master/systemd
 
@@ -61,89 +59,22 @@ function config_interface {
 }
 
 function config_do {
-	curl -Lo dnscrypt-proxy-$WHICHRESOLVER $GITURL/dnscrypt-config-$WHICHRESOLVER
-	if [ $DNSCRYPTINST == true ]; then
-		systemctl stop dnscrypt-proxy.service
-	fi
-	
-	mv dnscrypt-proxy-$WHICHRESOLVER /etc/conf.d/dnscrypt-proxy
-	systemctl enable dnscrypt-proxy.service
+	systemctl stop dnscrypt-proxy.service
+	curl -Lo /etc/conf.d/dnscrypt-config $GITURL/dnscrypt-config-$WHICHRESOLVER
 	systemctl start dnscrypt-proxy.service
 	return 0
 }
 
-function remove {
-	systemctl disable dnscrypt-proxy.service
-	pacman -R dnscrypt-proxy libsodium
-	chattr -i /etc/resolv.conf
-	mv /etc/resolv.conf-dnscryptbak /etc/resolv.conf
-}
+echo -e "\nWelcome to dnscrypt-autoinstall script.\n"
+echo -e "This will automatically configure DNSCrypt.\n"
+read -n1 -r -p "Press any key to continue..."
+clear
 
-# Checks package integrity, including /etc/conf.d/dnscrypt-proxy
-if pacman -Qk dnscrypt-proxy; then
-	DNSCRYPTINST=true
-fi
+echo -e "\nWould you like to see a list of supported providers?"
+read -p "(DNSCrypt.eu is default) [y/n]: " -e -i n SHOWLIST
 
-if pacman -Qk libsodium; then
-	LSODIUMINST=true
+if [ $SHOWLIST == "y" ]; then
+	config_interface
 fi
-
-if [ $DNSCRYPTINST == true ] && [ LSODIUMINST=true ]; then
-	echo "Welcome to dnscrypt-autoinstall script."
-	echo ""
-  	echo "It seems like DNSCrypt was installed and configured."
-	echo ""
-	echo "What would you like to do?"
-	echo ""
-	echo "1) Configure another DNSCrypt service"
-	echo "3) Exit"
-	echo ""
-		
-	read -p "Select an option [1-3]: " OPTION
-	case $OPTION in
-		1)
-			config_interface && config_do
-			echo "Reconfig done. Quitting."
-			exit
-			;;
-			
-		3)
-			echo "Bye!"
-			exit
-			;;
-	esac
-else
-	if nc -z -w1 127.0.0.1 53; then
-		echo ""
-		echo "Error!"
-		echo ""
-		echo "It looks like there is already a DNS server"
-		echo "or forwarder installed and listening on 127.0.0.1."
-		echo ""
-		echo "To use DNSCypt, you need to either uninstall it"
-		echo "or make it listen on another IP than 127.0.0.1."
-		echo ""
-		echo "Quitting."
-		exit 1
-		
-	else
-		echo ""
-		echo "Welcome to dnscrypt-autoinstall script."
-		echo ""
-		echo "This will install DNSCrypt and autoconfigure it to run as a daemon at start up."
-		echo ""
-		read -n1 -r -p "Press any key to continue..."
-		clear
-		echo ""
-		echo "Would you like to see a list of supported providers?"
-		
-		read -p "(DNSCrypt.eu is default) [y/n]: " -e -i n SHOWLIST
-		if [ $SHOWLIST == "y" ]; then
-			config_interface
-		fi
-		
-		# Set up configuration
-		config_do
-		
-	fi
-fi
+systemctl enable dnscrypt-proxy.service
+config_do
