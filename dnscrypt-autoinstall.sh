cfe23e47b793bd27c76cad42970eb7714794670f
andy@desk <andy@desk>
2014-07-30 09:42:45 +0800

    use wget
    curl make download failed on some slow network

--- dnscrypt-autoinstall.sh
+++ dnscrypt-autoinstall.sh
@@ -218,8 +218,10 @@ else
 		# Is libsodium installed?
 		if [ "$LSODIUMINST" == "false" ]; then
 			# Nope? Then let's get it set up
-			curl -o libsodium-$LSODIUMVER.tar.gz $LSODIUMURL
-			curl -o libsodium-$LSODIUMVER.tar.gz.sig $LSODIUMURL.sig
+			# use wget because curl make download fails on some slow network
+			# set retry & download timeout
+			wget -c -t3 -T60 --no-check-certificate -O libsodium-$LSODIUMVER.tar.gz $LSODIUMURL
+			wget -c -t2 -T10 --no-check-certificate -O libsodium-$LSODIUMVER.tar.gz.sig $LSODIUMURL.sig
 		
 			# Verify signature
 			verify_sig libsodium-$LSODIUMVER.tar.gz.sig libsodium-$LSODIUMVER.tar.gz
@@ -235,8 +237,8 @@ else
 		fi
 		
 		# Continue with dnscrypt installation
-		curl -o dnscrypt-proxy-$DNSCRYPTVER.tar.gz $DNSCRYPTURL
-		curl -o dnscrypt-proxy-$DNSCRYPTVER.tar.gz.sig $DNSCRYPTURL.sig
+		wget -c -t3 -T60 --no-check-certificate -O dnscrypt-proxy-$DNSCRYPTVER.tar.gz $DNSCRYPTURL
+		wget -c -t2 -T10 --no-check-certificate -O dnscrypt-proxy-$DNSCRYPTVER.tar.gz.sig $DNSCRYPTURL.sig
 		
 		# Verify signature
 		verify_sig dnscrypt-proxy-$DNSCRYPTVER.tar.gz.sig dnscrypt-proxy-$DNSCRYPTVER.tar.gz
