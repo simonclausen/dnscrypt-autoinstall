fa617b787f01a86dfa7f8f38abf726f1747afcaa
Alad Wenter <AladW@users.noreply.github.com>
2014-08-22 19:30:17 +0200

    update DNSCRYPTURL

--- dnscrypt-autoinstall-redhat.sh
+++ dnscrypt-autoinstall-redhat.sh
@@ -31,6 +31,7 @@ DNSCRYPTCONF=false
 LSODIUMVER=0.6.1
 DNSCRYPTVER=1.4.0
 LSODIUMURL="https://github.com/jedisct1/libsodium/releases/download/0.6.1"
+DNSCRYPTURL="https://github.com/jedisct1/dnscrypt-proxy/releases/download/1.4.0"
 DNSCRYPTURL="http://download.dnscrypt.org/dnscrypt-proxy"
 GPGURL_LS="https://download.libsodium.org/libsodium/releases"
 GPGURL_DC="http://download.dnscrypt.org/dnscrypt-proxy"
@@ -247,8 +248,8 @@ EOF
 		fi
 		
 		# Continue with dnscrypt installation 
-		curl -Lo dnscrypt-proxy-$DNSCRYPTVER.tar.gz $DNSCRYPTURL/dnscrypt-proxy-$DNSCRYPTVER.tar.gz
-		curl -Lo dnscrypt-proxy-$DNSCRYPTVER.tar.gz.sig $GPGURL_DC/dnscrypt-proxy-$DNSCRYPTVER.tar.gz.sig
+		curl -Lo dnscrypt-proxy-$DNSCRYPTVER.tar.gz $DNSCRYPTURL/dnscrypt-proxy-$DNSCRYPTVER.tar.bz2
+		curl -Lo dnscrypt-proxy-$DNSCRYPTVER.tar.gz.sig $GPGURL_DC/dnscrypt-proxy-$DNSCRYPTVER.tar.bz2.sig
 		
 		# Verify signature
 		verify_sig dnscrypt-proxy-$DNSCRYPTVER.tar.gz.sig
